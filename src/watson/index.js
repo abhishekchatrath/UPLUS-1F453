a15bffd8884d7e586c85914b4a24ae965ae1df13
Tonny Lay <tonny.lay@gmail.com>
2017-08-23 15:26:49 -0400

    return callback response from Watson

--- src/watson/index.js
+++ src/watson/index.js
@@ -5,7 +5,13 @@ var headers = {
     'Accept': 'application/json',
     'Authorization': 'Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRJZCI6ImE0ZDk5YmQwLWZiZGEtNDNlMC05MjdhLTRiODM3ZWExZjI5MiIsImluc3RhbmNlSWQiOiJhNGQ5OWJkMC1mYmRhLTQzZTAtOTI3YS00YjgzN2VhMWYyOTIiLCJwbGFuSWQiOiIzZjZhY2Y0My1lZGU4LTQxM2EtYWM2OS1mOGFmM2JiMGNiZmUiLCJyZWdpb24iOiJ1cy1zb3V0aCIsInVzZXJJZCI6IjBjMTZiNWVkLTk4ZjEtNDZmNS1iYWYwLTg3YTMwYTQ2ZDFmZSIsImlzcyI6Imh0dHA6Ly8xMjkuNDEuMjI5LjE4ODo4MDgwL3YyL2lkZW50aXR5IiwiaWF0IjoxNTAzNTA4ODcyLCJleHAiOjE1MDM1Mzc2NzJ9.gUp-uyN-8tnEgJvQoDXthKdkNWEW8fE_9YRBIHLOklofAPgXiWrpsyR1K468nuvt1jjQBh9lIGHRtux-sRzX_iSoFWEx1v5WfNe_3Qw32Q83gTIdd-M-NeV4dF_qBho9qHkJJPZhvMT6xQppXCKB_XgxTaT9Qj7rAkEYo4jC1iiuuyBt-f2KDKZBPULo1HTYY85JMizJg9apILOddJhPkRnY3YFoJUu3yHZpyHEpIPVScZGKUyBsr5b2SZ-UHTBivBX98IbrXpT8XOlxU7m5oKL3d7CMb9IiS7uVutzZax78tcMSG1oV1cDMgcEhOXxxry3zPyk3-0LRXXkOyRszAA'};
 
-var dataString = '{"fields": ["Name","Type"], "values": [["Agincourt","Cultural"]]}';
+var dataFormat = '{"fields": ["Name","Type"], "values": [["Agincourt","Cultural"]]}';
+
+function callback(error, response, body) {
+    console.log(JSON.parse(body).values[0][4]);
+    let text = JSON.parse(body).values[0][6].toString();
+    return text;
+}
 
 function getScore (branchName, programType, time) {
 	let dataString = '{"fields": ["Name","Type", "Time"], "values": [["' + branchName + '","' + programType + '","' + time + '"]]}';
@@ -16,7 +22,7 @@ function getScore (branchName, programType, time) {
     body: dataString
 };
 	console.log(dataString);
-	return 0.6;
+	return request(options, callback);
 }
 
 module.exports = Object.freeze(
